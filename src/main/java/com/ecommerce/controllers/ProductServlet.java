913d3d945cd8f237b1c99d9a36e3258accb7b0de
Charantej Akri <b.charantej@ajacs.in>
2025-11-15 23:55:29 +0530

    Refactor doGet method in ProductServlet

--- src/main/java/com/ecommerce/controllers/ProductServlet.java
+++ src/main/java/com/ecommerce/controllers/ProductServlet.java
@@ -1,26 +1,16 @@
 package com.ecommerce.controllers;
 
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ProductServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-
-        out.println("<html><body>");
-        out.println("<h1>E-Commerce Product List</h1>");
-        out.println("<ul>");
-        out.println("<li>Product 1 - ₹500</li>");
-        out.println("<li>Product 2 - ₹899</li>");
-        out.println("<li>Product 3 - ₹1299</li>");
-        out.println("</ul>");
-        out.println("</body></html>");
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Welcome to E-commerce Application!</h1>");
     }
 }
