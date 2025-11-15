87d9b55ca61fbb921f66960680fe4f9b0dac6f2a
Charantej Akri <b.charantej@ajacs.in>
2025-11-16 00:02:14 +0530

    Update ProductServlet.java

--- src/main/java/com/ecommerce/controllers/ProductServlet.java
+++ src/main/java/com/ecommerce/controllers/ProductServlet.java
@@ -1,16 +1,17 @@
 package com.ecommerce.controllers;
 
-import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.IOException;
 
 public class ProductServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        response.setContentType("text/html");
-        response.getWriter().println("<h1>Welcome to E-commerce Application!</h1>");
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Welcome to Ecommerce App!</h1>");
+        resp.getWriter().println("<p>Product list will appear here.</p>");
     }
 }
